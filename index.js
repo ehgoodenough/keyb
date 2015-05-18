63e9d72d6e6a9a5ace8007dc9e782bc9a8b4dce6
Andrew McPherson <andrewmcp333@gmail.com>
2015-05-18 08:22:13 -0400

    Added a method for "justDown"

--- index.js
+++ index.js
@@ -2,16 +2,44 @@ var vkey = require("vkey")
 
 var Keyb = {
     isDown: function(key) {
-        return this.data[key] === true
+        if(this.data[key] == undefined) {
+            this.data[key] = -1
+        }
+        return this.data[key] >= 0
+    },
+    isJustDown: function(key) {
+        if(this.data[key] == undefined) {
+            this.data[key] = -1
+        }
+        if(this.data[key] == 0) {
+            this.data[key] += 1
+            return true
+        } else {
+            return false
+        }
     },
     isUp: function(key) {
-        return this.data[key] !== true
+        if(this.data[key] == undefined) {
+            this.data[key] = -1
+        }
+        return this.data[key] <= 0
+    },
+    isJustUp: function(key) {
+        if(this.data[key] == undefined) {
+            this.data[key] = -1
+        }
+        if(this.data[key] == -1) {
+            this.data[key] -= 1
+            return true
+        } else {
+            return false
+        }
     },
     setDown: function(key) {
-        this.data[key] = true
+        this.data[key] = 0
     },
     setUp: function(key) {
-        delete this.data[key]
+        this.data[key] = -1
     },
     data: new Object()
 }
