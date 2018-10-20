65791e6d7f9fb79a769b207e81dbab72d837b891
Andrew McPherson <ehgoodenough@gmail.com>
2018-10-20 13:46:24 -0700

    Changed the API from isDown to isPressed.

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-var vkey = require("vkey")
+const vkey = require("vkey")
 
-var Keyb = {
-    isDown: function(key) {
+const Keyb = {
+    isPressed: function(key) {
         return this.data[key] != undefined
     },
-    isJustDown: function(key, delta) {
+    wasJustPressed: function(key, delta) {
         return window.performance.now() - this.data[key] < (delta || 1000 / 60)
     },
-    isUp: function(key) {
+    isNotPressed: function(key) {
         return this.data[key] == undefined
     },
-    setDown: function(key) {
+    setPressed: function(key) {
         this.data[key] = window.performance.now()
     },
-    setUp: function(key) {
+    setNotPressed: function(key) {
         delete this.data[key]
     },
     data: new Object()
 }
 
+Keyb.isDown = Keyb.isPressed
+Keyb.isJustDown = Keyb.wasJustPressed
+Keyb.isUp = Keyb.isNotPressed
+
 document.addEventListener("keydown", function(event) {
-    if(Keyb.isUp(vkey[event.keyCode])) {
-        Keyb.setDown(vkey[event.keyCode])
+    if(Keyb.isNotPressed(vkey[event.keyCode])) {
+        Keyb.setPressed(vkey[event.keyCode])
     }
 })
 
 document.addEventListener("keyup", function(event) {
-    Keyb.setUp(vkey[event.keyCode])
+    Keyb.setNotPressed(vkey[event.keyCode])
 })
 
 module.exports = Keyb
