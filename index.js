0a3d77fe73aecb9c176d9ebc32289f367f51869b
Andrew McPherson <ehgoodenough@gmail.com>
2019-12-25 10:42:47 -0800

    Linted the code.

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 const vkey = require("vkey")
 
 const Keyb = {
-    isPressed: function(key) {
+    "data": {},
+    "isPressed": function(key) {
         return this.data[key] != undefined
     },
-    wasJustPressed: function(key, delta) {
+    "wasJustPressed": function(key, delta) {
         return window.performance.now() - this.data[key] < (delta || 1000 / 60)
     },
-    isNotPressed: function(key) {
+    "isNotPressed": function(key) {
         return this.data[key] == undefined
     },
-    setPressed: function(key) {
-        this.data[key] = window.performance.now()
+    "setPressed": function(keycode) {
+        const key = vkey[keycode]
+        if(this.isPressed(key) == false) {
+            this.data[key] = window.performance.now()
+        }
     },
-    setNotPressed: function(key) {
+    "setNotPressed": function(keycode) {
+        const key = vkey[keycode]
         delete this.data[key]
     },
-    data: new Object()
 }
 
 Keyb.isDown = Keyb.isPressed
 Keyb.isJustDown = Keyb.wasJustPressed
 Keyb.isUp = Keyb.isNotPressed
 
-document.addEventListener("keydown", function(event) {
-    if(Keyb.isNotPressed(vkey[event.keyCode])) {
-        Keyb.setPressed(vkey[event.keyCode])
-    }
-})
-
-document.addEventListener("keyup", function(event) {
-    Keyb.setNotPressed(vkey[event.keyCode])
-})
+document.addEventListener("keydown", (event) => Keyb.setPressed(event.keyCode))
+document.addEventListener("keyup", (event) => Keyb.setNotPressed(event.keyCode))
 
 module.exports = Keyb
